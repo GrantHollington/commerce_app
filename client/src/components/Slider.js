f4e1c0b7d5857f690a4f0540ac72ea3d10111f8b
GrantHollington <grant.j.hollington@gmail.com>
2021-12-03 15:44:23 +1100

    create and style slider

---
+++ client/src/components/Slider.js
@@ -0,0 +1,119 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    position: relative;
+    overflow: hidden;
+`;
+
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #fff7f7;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props => props.direction === "left" && "10px"};
+    right: ${props => props.direction === "right" && "10px"};
+    margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
+`;
+
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+`;
+
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+`;
+
+const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`;
+
+const Image = styled.img`
+    height: 80%;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`;
+
+const Title = styled.h1`
+    font-size: 70px;
+`;
+
+const Description = styled.p`
+    margin: 50px 0;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`
+
+const Slider = () => {
+    return (
+        <Container>
+            <Arrow direction="left" >
+                <ArrowLeftOutlined />
+            </Arrow>
+            <Wrapper>
+                <Slide>
+                    <ImgContainer>
+                        <Image src="https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"/>
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>SUMMER SALE!</Title>
+                        <Description>DON'T COMPROMISE ON STYLE!!! 30% OFF NEW ARRIVALS!!</Description>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+                <Slide>
+                    <ImgContainer>
+                        <Image src="https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"/>
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>SUMMER SALE!</Title>
+                        <Description>DON'T COMPROMISE ON STYLE!!! 30% OFF NEW ARRIVALS!!</Description>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+                <Slide>
+                    <ImgContainer>
+                        <Image src="https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"/>
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>SUMMER SALE!</Title>
+                        <Description>DON'T COMPROMISE ON STYLE!!! 30% OFF NEW ARRIVALS!!</Description>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+            </Wrapper>
+             <Arrow direction="right">
+                <ArrowRightOutlined />
+            </Arrow>
+        </Container>
+    )
+}
+
+export default Slider
