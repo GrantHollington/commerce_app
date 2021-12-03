98e613b72b42b3dfebe24613a41802f0baf4160e
GrantHollington <grant.j.hollington@gmail.com>
2021-12-03 17:55:37 +1100

    complete slider component

---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+{
+    id: 1,
+    img: "https://images.unsplash.com/photo-1564584217132-2271feaeb3c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE!!! 30% OFF NEW ARRIVALS!!",
+    bg: 'f5fafd'
+},
+{
+    id: 2,
+    img: "https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE!!! 30% OFF NEW ARRIVALS!!",
+    bg: 'fcf1ed'
+},    
+{
+    id: 3,
+    img: "https://images.unsplash.com/photo-1614676471928-2ed0ad1061a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=882&q=80",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE!!! 30% OFF NEW ARRIVALS!!",
+    bg: "fbf0f4"
+}
+]
