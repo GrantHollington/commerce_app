279cedf2e95b3443ad69012d573f684b918958c1
GrantHollington <grant.j.hollington@gmail.com>
2021-12-05 18:39:08 +1100

    Add footer component and styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Annoucement from '../components/Annoucement'
 import Categories from '../components/Categories'
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import NewsLetter from '../components/NewsLetter'
 import Products from '../components/Products'
@@ -16,6 +17,7 @@ function Home() {
             <Categories />
             <Products />
             <NewsLetter />
+            <Footer />
         </div>
     )
 }
