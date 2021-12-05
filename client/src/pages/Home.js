e8ecba95f653994d06c1213e1e642181d8a08075
GrantHollington <grant.j.hollington@gmail.com>
2021-12-05 17:36:53 +1100

    create newsletter component and styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Annoucement from '../components/Annoucement'
 import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
+import NewsLetter from '../components/NewsLetter'
 import Products from '../components/Products'
 import Slider from '../components/Slider'
 
@@ -14,6 +15,7 @@ function Home() {
             <Slider />
             <Categories />
             <Products />
+            <NewsLetter />
         </div>
     )
 }
