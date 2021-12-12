af2ed2d1917e2ec92c4f71a61124014cfccfd6c1
GrantHollington <grant.j.hollington@gmail.com>
2021-12-12 16:32:32 +1100

    Add responsive design to pages

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import Annoucement from "../components/Annoucement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
 `;
@@ -34,6 +35,7 @@ const TopButton = styled.button`
 `;
 
 const TopTexts = styled.div`
+     ${mobile({ display: "none" })}
 `
 
 const TopText = styled.span`
@@ -45,6 +47,7 @@ const TopText = styled.span`
 const Bottom = styled.div`
     display: flex;
     justify-content: space-between;
+     ${mobile({ flexDirection: "column" })}
 `;
 
 const Info = styled.div`
@@ -55,6 +58,7 @@ const Info = styled.div`
 const Product = styled.div`
     display: flex;
     justify-content: space-between;  
+     ${mobile({ flexDirection: "column" })}
     `;
 
 const ProductDetail = styled.div`
@@ -103,11 +107,13 @@ const ProductAmountContainer = styled.div`
 const ProductAmount = styled.div`
     font-size: 24px;
     margin: 5px;
+    ${mobile({ margin: "5px 15px" })}
     `;
 
 const ProductPrice = styled.div`
     font-size: 30px;
     font-weight: 200;
+    ${mobile({ marginBottom: "20px" })}
     `;
 
 const Hr = styled.hr`
--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import NewsLetter from "../components/NewsLetter";
 import Footer from "../components/Footer";
 import { Add, Remove } from "@material-ui/icons";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
 `;
@@ -11,6 +12,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 50px;
     display: flex;
+    ${mobile({ padding: "10px", flexDirection: "column" })}
 `;
 
 const ImgContainter = styled.div`
@@ -21,11 +23,13 @@ const Image = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: cover;
+    ${mobile({ height: "40%" })}
 `;
 
 const InfoContainer = styled.div`
     flex: 1;
     padding: 0px 50px;
+     ${mobile({ padding: "10px" })}
 `;
 
 const Title = styled.h1`
@@ -46,6 +50,7 @@ const FilterContainer = styled.div`
     margin: 30px 0px;
     display: flex;
     justify-content: space-between;
+    ${mobile({ width: "100%" })}
 `;
 
 const Filter = styled.div`
@@ -79,6 +84,7 @@ const AddContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    ${mobile({ width: "100%" })}
 `;
 
 const AmountContainer = styled.div`
@@ -105,7 +111,7 @@ const Button = styled.button`
     cursor: pointer;
     font-weight: 500;
 
-    &hover:{
+    &:hover{
         background-color: #f8f4f4;
     }
 `;
