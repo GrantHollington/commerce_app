9cd7a48c8b043f976ec5417fb4d4971167be34a9
GrantHollington <grant.j.hollington@gmail.com>
2021-12-28 16:19:33 +1100

    add dummydata and props to chart

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import Chart from "../../components/chart/Chart"
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo"
 import "../home/home.css"
+import { userData } from "../../dummyData"
 
 export default function Home() {
     return (
         <div className='home'>
             <FeaturedInfo />
-            <Chart />
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+            <div className="homeWidgets"></div>
         </div>
     )
 }
