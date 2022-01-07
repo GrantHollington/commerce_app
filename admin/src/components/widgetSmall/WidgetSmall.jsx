6d2078f3b2cca94e6839344be6c2f72b6dd556c0
GrantHollington <grant.j.hollington@gmail.com>
2022-01-07 22:34:46 +1100

    update small widget, request methods and user model in api

--- admin/src/components/widgetSmall/WidgetSmall.jsx
+++ admin/src/components/widgetSmall/WidgetSmall.jsx
@@ -1,22 +1,34 @@
 import "./widgetSmall.css";
 import { Visibility } from "@material-ui/icons";
+import { useState, useEffect } from "react";
+import { userRequest } from "../../redux/requestMethods";
 
 export default function WidgetSmall() {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const res = await userRequest.get("users/?new=true")
+            setUsers(res.data)
+        }
+        getUsers();
+    }, [])
     return (
         <div className="widgetSmall">
             <span className="widgetSmallTitle">New Join Members</span>
             <ul className="widgetSmallList">
-                <li className="widgetSmallListItem">
-                    <img src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif" alt="" className="widgetSmallImage" />
+                {users.map(user => (
+                    <li className="widgetSmallListItem" key={user._id}>
+                    <img src={user.img || "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"} alt="" className="widgetSmallImage" />
                     <div className="widgetSmallUser">
-                        <span className="widgetSmallUsername">Hellen Keller</span>
-                        <span className="widgetSmallUserTitle">Software Engineer</span>
+                        <span className="widgetSmallUsername">{user.username}</span>
                     </div>
                     <button className="widgetSmallButton">
                         <Visibility className="widgetSmallIcon"/>
                         Display
                     </button>
                 </li>
+                ))}
             </ul>
         </div>
     )
