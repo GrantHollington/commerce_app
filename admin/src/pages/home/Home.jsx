80fe512552bb1246fb747b3cf219b0c9f728e874
GrantHollington <grant.j.hollington@gmail.com>
2022-01-08 11:23:00 +1100

    add useState and Useeffect to admin homepage

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -4,12 +4,53 @@ import "./home.css";
 import { userData } from "../../dummyData";
 import WidgetSmall from "../../components/widgetSmall/WidgetSmall";
 import WidgetLarge from "../../components/widgetLarge/WidgetLarge";
+import { useState, useMemo, useEffect } from "react";
+import { userRequest } from "../../redux/requestMethods";
 
 export default function Home() {
+    const [userStats, setUserStats] = useState([])
+
+    const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+    useEffect(() => {
+        const getStats = async () =>{
+            try {
+               const res = await userRequest.get("/users.stats")
+               res.data.map(item => {
+                   setUserStats(prev => [
+                       ...prev,
+                       {name:MONTHS[item._id-1], "Active User": item.total}
+                   ])
+               }) 
+            } catch (error) {
+                
+            }
+        };
+        getStats(); 
+    },[MONTHS]);
+
+    console.log(userStats)
+
     return (
         <div className='home'>
             <FeaturedInfo />
-            <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+            <Chart data={userStats} title="User Analytics" grid dataKey="Active User" />
             <div className="homeWidgets">
                 <WidgetSmall />
                 <WidgetLarge />
