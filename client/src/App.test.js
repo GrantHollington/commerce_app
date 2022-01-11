71218a0a2549e8a05d6fdc90a13f9da34e5dc067
GrantHollington <grant.j.hollington@gmail.com>
2022-01-11 18:29:00 +1100

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders basic empty template', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/basic empty template/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders basic empty template', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/basic empty template/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
